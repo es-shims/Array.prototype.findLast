3483d489b0a2c17509d34aa386de8b83d4c6c0a1
Jordan Harband <ljharb@gmail.com>
2021-08-07 10:24:38 -0700

    [Fix] ES3 engines: ensure nullish receiver throws

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 var define = require('define-properties');
 var callBind = require('call-bind');
+var callBound = require('call-bind/callBound');
+var RequireObjectCoercible = require('es-abstract/2020/RequireObjectCoercible');
 
 var implementation = require('./implementation');
 var getPolyfill = require('./polyfill');
 var polyfill = getPolyfill();
 var shim = require('./shim');
 
-var bound = callBind(polyfill);
+var $slice = callBound('Array.prototype.slice');
 
-define(bound, {
+var bound = callBind.apply(polyfill);
+// eslint-disable-next-line no-unused-vars
+var boundFindLast = function findLast(array, predicate) {
+	RequireObjectCoercible(array);
+	return bound(array, $slice(arguments, 1));
+};
+
+define(boundFindLast, {
 	getPolyfill: getPolyfill,
 	implementation: implementation,
 	shim: shim
 });
 
-module.exports = bound;
+module.exports = boundFindLast;
